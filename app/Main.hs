188b10a9fc5395f956e0d5ca22d8493d25aae26d
Martin Muggli <martin.muggli@gmail.com>
2021-02-11 07:51:19 -0700

    encode kmers to ints; Intmap based implementation of map

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,14 @@ module Main where
 
 import Lib
 import qualified Data.ByteString.Lazy.Char8 as L
-import qualified Data.Map as M
+--import qualified Data.ByteString.Lazy.UTF8 as BSLU
+--import qualified Data.Map as M
+import qualified Data.IntMap.Strict as M    
 import Data.List
 import qualified Math.Combinatorics.Multiset as MS
 import Debug.Trace
-    
+import Data.Bits
+import Data.Word
 -- https://stackoverflow.com/questions/3710976/counting-unique-elements-in-a-list
 
 --countElems4 = MS.toCounts . MS.fromList
@@ -28,25 +31,51 @@ k = 31
 windows n xs = filter ((>= k) . L.length) $ map (L.take n) (L.tails xs)
 
 -- try https://stackoverflow.com/questions/13758704/haskell-is-there-a-standard-function-to-provide-a-count-of-each-item-in-a-list               
-countElems :: (Ord a) => [a] -> M.Map a Int
-countElems = M.fromListWith (+) . flip zip (repeat 1)
+-- countElems :: (Ord a) => [a] -> M.Map a Int
+-- countElems = M.fromListWith (+) . flip zip (repeat 1)
 
 --buildMap = countElems
-buildMap :: [L.ByteString] -> M.Map L.ByteString Int
+buildMap ::  [Int] -> M.IntMap  Int
 buildMap bsl = foldl' f M.empty bsl
              where f amap bs = let res = M.insertWith (+) bs 1 amap in
                                if M.size amap `mod` 1000000 == 0
                                then trace (show $ M.size amap) res
                                else res
+-- intOfBase :: Word8 -> Int
+-- intOfBase 65 = 0
+-- intOfBase 67 = 1
+-- intOfBase 71 = 2
+-- intOfBase 84 = 3
 
+intOfBase :: Char -> Int
+intOfBase 'A' = 0
+intOfBase 'C' = 1
+intOfBase 'G' = 2
+intOfBase 'T' = 3
+               
+encodeBases :: L.ByteString -> Int                
+encodeBases bases = L.foldl  (\a b -> (a `shiftL` 2) .|. (intOfBase b)) 0 $  bases                
+                                    
+-- encodeInto :: Int -> L.ByteString -> Int
+-- --encodeInto i [] = i
+-- encodeInto i bs = let c = take 1 bs
+--                       cs = drop 1 bs in
+--                   case c of
+--                         'A' -> encodeInto (i `shiftL` 2) cs
+--                         'C' -> encodeInto ((i `shiftL` 2) .|. 1) cs
+--                         'G' -> encodeInto ((i `shiftL` 2) .|. 2) cs
+--                         'T' -> encodeInto ((i `shiftL` 2) .|. 3) cs                               
+
+                                    
 -- TODO filter k-mers with 'N's
 -- TODO count canonical k-mers
 
-complement :: Char -> Char
-complement 'C' = 'G'
-complement 'G' = 'C'
-complement 'A' = 'T'
-complement otherwise = 'A'
+complementBase :: Char -> Char
+complementBase 'C' = 'G'
+complementBase 'G' = 'C'
+complementBase 'A' = 'T'
+complementBase 'T' = 'A'
+complementBase _ = error "Non DNA letter found"
 
 data FASTALine  = Header  | Sequence  | QualDelim | Quality
                   deriving (Eq, Show)
@@ -72,7 +101,7 @@ canonical x = let rc = revcomp x in
                   
                
 revcomp :: L.ByteString -> L.ByteString
-revcomp bs = L.reverse $ L.map complement bs
+revcomp bs = L.reverse $ L.map complementBase bs
 
 noNs :: L.ByteString -> Bool
 noNs bs = case L.find ('N' == ) bs of
@@ -97,7 +126,8 @@ main =  do contents <- L.getContents
            let kmers = concat $ map (windows k) sequences
            let filteredKmers = filter noNs kmers
            let canonicalKmers = fmap canonical filteredKmers
-           let mymap = buildMap canonicalKmers
+           let encoded = fmap encodeBases  canonicalKmers
+           let mymap = buildMap encoded 
 --           let counted = map (\x -> (length x, head x)) $ group . sort $ kmers
            putStrLn (show $ M.size mymap) -- 
 --           mapM (putStrLn . show) counted
