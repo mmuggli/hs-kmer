b0f7f17ec8c6cb84092fe835d193bf4c9fcf6d5b
Martin Muggli <martin.muggli@gmail.com>
2021-02-11 09:22:33 -0700

    cleanup a bit

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Lib
-import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.ByteString.Char8 as L
 --import qualified Data.ByteString.Lazy.UTF8 as BSLU
 --import qualified Data.Map as M
 import qualified Data.IntMap.Strict as M    
@@ -38,8 +38,8 @@ windows n xs = filter ((>= k) . L.length) $ map (L.take n) (L.tails xs)
 buildMap ::  [Int] -> M.IntMap  Int
 buildMap bsl = foldl' f M.empty bsl
              where f amap bs = let res = M.insertWith (+) bs 1 amap in
-                               if M.size amap `mod` 1000000 == 0
-                               then trace (show $ M.size amap) res
+                               if M.size amap `mod` 10000 == 0
+                               then trace ("Map entries: " ++ ( show $ M.size amap)) res
                                else res
 -- intOfBase :: Word8 -> Int
 -- intOfBase 65 = 0
@@ -56,52 +56,42 @@ intOfBase 'T' = 3
 encodeBases :: L.ByteString -> Int                
 encodeBases bases = L.foldl  (\a b -> (a `shiftL` 2) .|. (intOfBase b)) 0 $  bases                
                                     
--- encodeInto :: Int -> L.ByteString -> Int
--- --encodeInto i [] = i
--- encodeInto i bs = let c = take 1 bs
---                       cs = drop 1 bs in
---                   case c of
---                         'A' -> encodeInto (i `shiftL` 2) cs
---                         'C' -> encodeInto ((i `shiftL` 2) .|. 1) cs
---                         'G' -> encodeInto ((i `shiftL` 2) .|. 2) cs
---                         'T' -> encodeInto ((i `shiftL` 2) .|. 3) cs                               
 
-                                    
--- TODO filter k-mers with 'N's
--- TODO count canonical k-mers
-
-complementBase :: Char -> Char
-complementBase 'C' = 'G'
-complementBase 'G' = 'C'
-complementBase 'A' = 'T'
-complementBase 'T' = 'A'
-complementBase _ = error "Non DNA letter found"
+complementOfBase :: Char -> Char
+complementOfBase 'C' = 'G'
+complementOfBase 'G' = 'C'
+complementOfBase 'A' = 'T'
+complementOfBase 'T' = 'A'
+complementOfBase _ = error "Non DNA letter found"
 
 data FASTALine  = Header  | Sequence  | QualDelim | Quality
                   deriving (Eq, Show)
                            
 -- Ghetto parsing, refactor to be less repetitive
-tag :: FASTALine -> [L.ByteString] -> [(FASTALine, L.ByteString)]
+tag :: FASTALine -> [(L.ByteString, Int)] -> [(FASTALine, L.ByteString)]
 tag _ [] = []       
-tag lastlinetype (line:lines) = case (L.head line) of
-                                  '>' -> (Header, line) : tag Header lines
-                                  '@' -> (Header, line) : tag Header lines
-                                  '+' -> (QualDelim, line) : tag QualDelim lines
-                                  otherwise -> case lastlinetype of
+tag lastlinetype ((line, lineNo):lines) = if (lineNo `mod` 1000) == 0
+                                          then trace ("Line " ++ (show lineNo)) $ doit ((line, lineNo):lines)
+                                          else doit ((line, lineNo):lines)
+    where doit ((line, lineNo):lines) = case (L.head line) of
+                                          '>' -> (Header, line) : tag Header lines
+                                          '@' -> (Header, line) : tag Header lines
+                                          '+' -> (QualDelim, line) : tag QualDelim lines
+                                          otherwise -> case lastlinetype of
                                                  Header -> (Sequence, line) : ( tag Sequence lines)
                                                  Sequence -> (Sequence, line) : ( tag Sequence lines )
                                                  QualDelim -> (Quality, line) : ( tag Quality lines )
                                                  Quality -> (Quality, line) : ( tag Quality lines )
     
 -- TODO we should be able to decide if the revcomp is canonical without computing the entire revcomp, do that               
-canonical x = let rc = revcomp x in
+canonicalize x = let rc = revcomp x in
               if rc < x
               then rc
               else x
                   
                
 revcomp :: L.ByteString -> L.ByteString
-revcomp bs = L.reverse $ L.map complementBase bs
+revcomp bs = L.reverse $ L.map complementOfBase bs
 
 noNs :: L.ByteString -> Bool
 noNs bs = case L.find ('N' == ) bs of
@@ -113,24 +103,20 @@ noNs bs = case L.find ('N' == ) bs of
 main :: IO ()
 main =  do contents <- L.getContents
            let mylines = L.lines contents
-
-           let taggedLines = tag Quality mylines
-           let groups1 = groupBy equalTags taggedLines
+           let numberedLines = zip mylines [1..]
+           let taggedLines = tag Quality numberedLines
+           let groupedByTag = groupBy equalTags taggedLines
                    where
                      equalTags (tag1, _) (tag2, _) = tag1 == tag2
-           -- let groups1 = groupBy (\a b -> ((L.head a) == '>') == ((L.head b) == '>')) mylines
-           --         where
-           let groups2 = filter ((Sequence ==)  . fst . head) groups1
-           let sequences = map (L.concat . (fmap snd))  groups2
-           --mapM (putStrLn . show) sequences                           
+           let sequenceGroups = filter ((Sequence ==)  . fst . head) groupedByTag
+           let sequences = map (L.concat . (fmap snd))  sequenceGroups
            let kmers = concat $ map (windows k) sequences
            let filteredKmers = filter noNs kmers
-           let canonicalKmers = fmap canonical filteredKmers
-           let encoded = fmap encodeBases  canonicalKmers
-           let mymap = buildMap encoded 
---           let counted = map (\x -> (length x, head x)) $ group . sort $ kmers
+           let canonicalKmers = fmap canonicalize filteredKmers
+           let encodedKmers = fmap encodeBases  canonicalKmers
+           let mymap = buildMap encodedKmers
+
+
            putStrLn (show $ M.size mymap) -- 
---           mapM (putStrLn . show) counted
---           mapM (putStrLn . show) kmers
---           putStrLn (show kmers)
+
            return ()
